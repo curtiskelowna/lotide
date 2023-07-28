fe42d75f299e71e9ac5c72bb8e645333df3b5708
Curtis Platt <curtisplatt@hotmail.com>
2023-07-28 15:40:36 -0400

    without.js

--- without.js
+++ without.js
@@ -1,18 +1,30 @@
 // FUNCTION IMPLEMENTATION
 const eqArrays = function(array1, array2) {
-  console.log(array1, array2);
   // if the arrays are not the same length, return false
   if (array1.length !== array2.length) {
     return false;
   }
+  // loop through the arrays assuming they are the same length
+  for (let i = 0; i < array1.length; i++) {
+    // if the elements are not the same, return false
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  // if the elements are the same, return true
+  return true;
 };
+
+// FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(array1, array2) {
-  console.log(array1, array2);
-  // if the arrays are not the same length, return false
-  if (array1.length !== array2.length) {
-    return false;
+const result = eqArrays(array1, array2);
+  if (result === true) {
+    console.log(`true, ${array1} === ${array2}`)
+  } else {
+    console.log(`false, ${array1} !== ${array2}`)
   }
 };
+
 // FUNCTION IMPLEMENTATION
 const without = function(sourceArray, itemsToRemoveArray) {
   let newArray = [];
