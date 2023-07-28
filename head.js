a005c975bddb9a9dd74507ccea71756c50c9a894
Curtis Platt <curtisplatt@hotmail.com>
2023-07-28 14:46:22 -0400

    head.js

--- head.js
+++ head.js
@@ -6,6 +6,7 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+// FUNCTION IMPLEMENTATION
 const head = function(array) {
   return array[0];
 };
