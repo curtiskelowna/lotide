21a56b09b89e5953d00722e1229e17e104fd098b
Curtis Platt <curtisplatt@hotmail.com>
2023-07-28 14:28:25 -0400

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log('✅✅✅Assertion Passed: ${actual} === ${expected}');
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(array1, array2) {
   console.log(array1, array2);
   // if the arrays are not the same length, return false
@@ -13,10 +6,14 @@ const eqArrays = function(array1, array2) {
     return false;
   }
 };
-
+const assertArraysEqual = function(array1, array2) {
+  console.log(array1, array2);
+  // if the arrays are not the same length, return false
+  if (array1.length !== array2.length) {
+    return false;
+  }
+};
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
