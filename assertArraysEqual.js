2d16d272a52bfce4e072516a065215d124b14a2e
Curtis Platt <curtisplatt@hotmail.com>
2023-07-28 15:38:28 -0400

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,33 @@
 // FUNCTION IMPLEMENTATION
 const eqArrays = function(array1, array2) {
-  console.log(array1, array2);
   // if the arrays are not the same length, return false
   if (array1.length !== array2.length) {
     return false;
   }
+  // loop through the arrays assuming they are the same length
+  for (let i = 0; i < array1.length; i++) {
+    // if the elements are not the same, return false
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  // if the elements are the same, return true
+  return true;
 };
+
+// FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(array1, array2) {
-  console.log(array1, array2);
-  // if the arrays are not the same length, return false
-  if (array1.length !== array2.length) {
-    return false;
+const result = eqArrays(array1, array2);
+  if (result === true) {
+    console.log(`true, ${array1} === ${array2}`)
+  } else {
+    console.log(`false, ${array1} !== ${array2}`)
   }
 };
+
 // TEST CODE
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
