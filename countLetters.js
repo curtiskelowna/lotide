2402453b9e317e7ca71c2d8bef8cb69912196952
Curtis Platt <curtisplatt@hotmail.com>
2023-07-27 21:26:44 -0400

    countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,14 @@
 // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅Assertion Passed: ${actual} === ${expected}');
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// FUNCTION IMPLEMENTATION
 const countLetters = function(sentence) {
 
   let senLowerCase = sentence.toLowerCase();
@@ -17,3 +27,13 @@ const countLetters = function(sentence) {
 
 let string = "Lighthouse in the house";
 console.log(countLetters(string));
+assertEqual(countLetters(string)["l"], 1);
+assertEqual(countLetters(string)["i"], 2);
+assertEqual(countLetters(string)["g"], 1);
+assertEqual(countLetters(string)["h"], 4);
+assertEqual(countLetters(string)["t"], 2);
+assertEqual(countLetters(string)["o"], 2);
+assertEqual(countLetters(string)["u"], 2);
+assertEqual(countLetters(string)["s"], 2);
+assertEqual(countLetters(string)["e"], 3);
+assertEqual(countLetters(string)["n"], 1);
