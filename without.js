3c7cf5c4808175c6edc50cda0d209891d969a4c0
Curtis Platt <curtisplatt@hotmail.com>
2023-07-28 14:31:02 -0400

    without.js

--- without.js
+++ without.js
@@ -1,11 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log('✅✅✅Assertion Passed: ${actual} === ${expected}');
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const eqArrays = function(array1, array2) {
   console.log(array1, array2);
   // if the arrays are not the same length, return false
@@ -35,12 +28,3 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-// TEST CODE
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
