b65ff63fe5e4a15f8e19e6e6fc5ae299d22bc079
Curtis Platt <curtisplatt@hotmail.com>
2023-07-29 12:05:59 -0400

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -26,8 +26,7 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 // TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
