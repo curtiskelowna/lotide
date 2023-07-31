2574063823c1b402d671d55f1f71695cb95d68e2
Curtis Platt <curtisplatt@hotmail.com>
2023-07-31 15:56:35 -0400

    middle.js refactored

--- middle.js
+++ middle.js
@@ -1,29 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  // if the arrays are not the same length, return false
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  // loop through the arrays assuming they are the same length
-  for (let i = 0; i < array1.length; i++) {
-    // if the elements are not the same, return false
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  // if the elements are the same, return true
-  return true;
-};
-
-// FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(actual, expected) {
-  const result = eqArrays(actual, expected);
-  if (result === true) {
-    console.log(`true, ${actual} === ${expected}`);
-  } else {
-    console.log(`false, ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // FUNCTION IMPLEMENTATION
 const middle = function(array) {
@@ -38,10 +14,5 @@ const middle = function(array) {
   }
   return newArray;
 };
-// TEST CODE
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3]); // => [3]
+
+module.exports = middle;
